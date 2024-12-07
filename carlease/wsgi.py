7d9522fcda803d9aed92d88f5f7694a766313b4c
oma <oesgibone@gmail.com>
2024-12-07 10:32:26 +0100

    changes for render

--- carlease/wsgi.py
+++ carlease/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AutoM.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carlease.settings')
 
 application = get_wsgi_application()
